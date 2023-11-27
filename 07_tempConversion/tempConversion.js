01a46d09bee56015434d623ee9ce5a1150dd1898
Yaniss G.M. Chioukh <chioukh.yanis@hotmail.fr>
2023-11-27 18:20:56 +0000

    add solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahreinheit) {
+  tempCelsius = (fahreinheit - 32) * 5/9;
+  return parseFloat(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  tempFahreinheit = celcius * 9/5 + 32;
+  return parseFloat(tempFahreinheit.toFixed(1));
 };
 
 // Do not edit below this line
