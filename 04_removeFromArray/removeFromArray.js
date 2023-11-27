435de91214f483c1b544bcf799ce5f17a7583e04
Yaniss G.M. Chioukh <chioukh.yanis@hotmail.fr>
2023-11-27 15:57:26 +0000

    add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...argsToRemove) {
+    const myArray = [];
+    array.forEach(item => {
+        if (!argsToRemove.includes(item)) {
+            myArray.push(item);
+        }
+    });
+    return myArray;
 };
 
 // Do not edit below this line
